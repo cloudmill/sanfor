9891fb32ab26a7eebdc534ca8830fb5770caf300
tj <andrei.tabalin@dterra.eu>
2018-09-27 14:05:48 +0300

    dev обновление верстки до v0.96 beta
    Добавлена страница о компании, анимации кнопок при ховере

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,45 @@
 var SITE_TEMPLATE_PATH = "/local/templates/sanfor";
 
+// disable scroll
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+    e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+    window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+  if (window.removeEventListener)
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.onmousewheel = document.onmousewheel = null; 
+  window.onwheel = null; 
+  window.ontouchmove = null;  
+  document.onkeydown = null;  
+}
+
+// disable scroll
+
 $(document).ready(function () {
   $('.name span').click(function() {
     $('.name span').removeClass('active');
@@ -8,8 +48,32 @@ $(document).ready(function () {
     $(this).parents('.name').siblings('.block').children('.item').eq($(this).index()).addClass('active');
   });
 
+  var link = window.location.href;
+  link = link.split("/");
+  if (link[3] == "production" && link[4] == "") {
+    var offset = $(window).height() - $(".wrapper.catalog main .inner .catalog-content .products .pages").offset().top - $(".wrapper.catalog main .inner .catalog-content .products .pages").height();
+    $(document).on("scroll", function() {
+      var positive_offset = Math.abs(offset);
+      var bottom = $(document).scrollTop() + offset;
+      console.log(bottom);
+      if (bottom >= 0) {
+        $(".wrapper.catalog main .inner .sidebar").addClass("scrolled");
+        $(".wrapper.catalog main .inner .sidebar").css("top", positive_offset);
+      }
+      else {
+        $(".wrapper.catalog main .inner .sidebar").removeClass("scrolled");
+        $(".wrapper.catalog main .inner .sidebar").css("top", 0);
+      }
+    });
+  }
 
   $(".mob_btn").on("click", function() {
+    if ($(this).hasClass("open")) {
+      enableScroll();
+    }
+    else {
+      disableScroll();
+    }
     $(this).toggleClass("open");
     $("body").toggleClass("noscroll");
     $("header").toggleClass("active");
@@ -36,6 +100,28 @@ $(document).ready(function () {
       el: '.swiper-pagination',
       type: 'fraction',
     },
+  });
+
+  var mySwiper_evo = new Swiper ('.evo-blog-swiper', {
+    loop: true,
+    simulateTouch: false,
+    spaceBetween: 52,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  var mySwiper_serts = new Swiper ('.serts-blog-swiper', {
+    loop: true,
+    simulateTouch: false,
+    spaceBetween: 100,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
   });
 
   var mySwiper_blog = new Swiper ('.blog-swiper-container', {
